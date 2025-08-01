aecbef0edeafb3f39c8d077c922f3eabee8ae37c
Alexander Baier <alexander.baier@rena.com>
2025-08-01 11:05:52 +0200

    created component thats capable of displaying a list of row headers next to the chart

--- src/components/PlanningTool/components/EmployeeChart.vue
+++ src/components/PlanningTool/components/EmployeeChart.vue
@@ -1,44 +1,45 @@
 <template>
-    <Gantt v-model="chartData"></Gantt>
+    <ResourceGantt v-model="chartData"></ResourceGantt>
 </template>
 
 <script setup>
 import { onMounted, reactive, ref } from 'vue';
 import { store } from '@/store/store';
-import Gantt from '../utility_components/Gantt.vue';
 import axios from 'axios';
 import { ChartItem, ChartItemType } from 'gantt-planner';
+import ResourceGantt from '../utility_components/ResourceGantt.vue';
 
-const chartData = ref([])
+const chartData = ref({keys: [], chart: []})
 
 async function LoadEmployeeAssignments() {
     try{
         const response = await axios.get('http://localhost:8080/api/employees')
         if(response.status !== 200){
-            chartData.value = []
+            chartData.value = {keys: [], chart: []}
             return;
         }
 
         const result = []
+        const keys = new Map()
         for (let employee of response.data){
             
             const assignmentResponse = await axios.get(`http://localhost:8080/api/assignments/${employee.employeeId}`)
             if(assignmentResponse.status !== 200){
-                chartData.value = []
+                chartData.value = {keys: [], chart: []}
                 return
             }
 
             for(let assignment of assignmentResponse.data){
                 const taskResponse = await axios.get(`http://localhost:8080/api/tasks/${assignment.taskId}`)
                 if(taskResponse.status !== 200){
-                    chartData.value = []
+                    chartData.value = {keys: [], chart: []}
                     return
                 }
                 const task = taskResponse.data[0]
 
                 const projectResponse = await axios.get(`http://localhost:8080/api/projects/${task.prjId}`)
                 if(projectResponse.status !== 200){
-                    chartData.value = []
+                    chartData.value = {keys: [], chart: []}
                     return
                 }
                 const prj = projectResponse.data[0]
@@ -54,11 +55,14 @@ async function LoadEmployeeAssignments() {
                         prj.view.color,
                     )
                 )
+
+                if(!keys.has(employee.employeeId))
+                    keys.set(employee.employeeId, employee.name)                
             }
          
         }
-    
-        chartData.value = result
+        
+        chartData.value = { keys: Array.from(keys.values()).sort(), chart: result }
     }
     catch(error){
         console.log(error)
---
+++ src/components/PlanningTool/utility_components/ResourceGantt.vue
@@ -0,0 +1,155 @@
+<template>
+    <div class="resource-gantt-area">
+        <div class="row-headers" ref="rowHeader">
+            <div ref="rowSearch"></div>
+            <div class="content-box" ref="contentBox">
+                <div ref="rowContent"></div>
+            </div>
+        </div>
+        <div class="chart-area" ref="ganttArea">
+            <div ref="ganttContainer"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import GanttChart from 'gantt-planner'
+import { onMounted, ref, watch } from 'vue';
+
+const props = defineProps({
+    modelValue: {
+        type: Object,
+        required: true
+    }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const ganttArea = ref(null)
+const ganttContainer = ref(null)
+const rowSearch = ref(null)
+const rowContent = ref(null)
+const rowHeader = ref(null)
+const contentBox = ref(null)
+
+const rowHeaderItems = ref([])
+const chartData = ref([])
+let instance = null
+
+watch(() => props.modelValue, (newVal) => {
+    chartData.value = newVal.chart
+    rowHeaderItems.value = newVal.keys
+    renderChart(chartData.value)
+})
+
+function renderChart(tasks){
+    if(!tasks)
+        tasks = []
+
+    if(ganttContainer.value){
+        ganttContainer.value.innerHTML = ''
+    }
+
+    const height = ganttArea.value?.offsetHeight || 400
+    instance = new GanttChart(ganttContainer.value, tasks, {
+        view_mode: 'Day',
+        date_format: 'DD-MM-YYYY',
+        scroll_to: 'today',
+        container_height: `${height}`
+    })
+
+    createRowHeaders()
+    syncScroll()
+}
+
+function createRowHeaders(){
+    if(rowContent.value)
+        rowContent.value.innerHTML = ''
+    if(rowSearch.value)
+        rowSearch.value.innerHTML = ''
+
+    if(!instance || !ganttContainer.value)
+        return
+
+    const chartHeader = ganttContainer.value.getElementsByClassName('grid-header')[0]
+    const chartHeaderHeight = chartHeader.offsetHeight
+
+    rowSearch.value.style.height = `${chartHeaderHeight}px`
+    rowContent.value.style.height = `${instance.getRowHeight() * instance.row_count}px`
+    contentBox.value.style.height = `${ganttArea.value.offsetHeight - chartHeaderHeight}px`
+
+    for (let index = 0; index < instance.row_count; index++){
+        let row = document.createElement('div')
+        row.style.position = 'relative'
+        row.style.width = `${rowHeader.value.offsetWidth}px`
+        row.style.height = `${instance.getRowHeight()}px`
+        row.style.borderBottom = 'solid 1px #ebeff2'
+
+        if(index < rowHeaderItems.value.length){
+            let it = rowHeaderItems.value[index]
+            let text = document.createElement('p')
+            text.appendChild(document.createTextNode(it))
+            text.style.padding = `${instance.options.padding / 2}px`
+            text.style.textAlign = 'center'
+            row.appendChild(text)
+        }
+
+        rowContent.value.appendChild(row)
+    }
+}
+
+function syncScroll(){
+    let ganttScroll = ganttContainer.value.getElementsByClassName('gantt-container')[0]
+    let rowHeaderScroll = contentBox.value
+
+    ganttScroll.onscroll = () => {
+        let ganttScroll = ganttContainer.value.getElementsByClassName('gantt-container')[0]
+        let rowHeaderScroll = contentBox.value
+
+        rowHeaderScroll.scrollTop = ganttScroll.scrollTop
+    }
+
+    rowHeaderScroll.onscroll = () => {
+        let ganttScroll = ganttContainer.value.getElementsByClassName('gantt-container')[0]
+        let rowHeaderScroll = contentBox.value
+
+        ganttScroll.scrollTop = rowHeaderScroll.scrollTop
+    }
+}
+
+onMounted(() => {
+    const observer = new ResizeObserver(() => {
+        renderChart(chartData.value)
+    })
+    observer.observe(ganttArea.value)
+})
+
+
+</script>
+
+<style scoped>
+.resource-gantt-area{
+    height: 100%;
+    width: 100%;
+}
+
+.row-headers{
+    width: 20%;
+    float: left;
+    height: 100%;
+}
+
+.content-box{
+    overflow: auto;
+    border-top: solid 1px #ebeff2;
+    border-right: solid 1px #c7c7c7;
+}
+
+.chart-area{
+    display: block;
+    box-sizing: border-box;
+    width: 80%;
+    height: 100%;
+    float: right;
+}
+</style>
